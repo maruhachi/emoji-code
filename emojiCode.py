2b712d50b62b19a0c8d8e5ed366b5a409f6e948a
ykonno <konchan1129@gmail.com>
2019-12-16 15:17:36 +0900

    Pythonで絵文字コーディング

---
+++ emojiCode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class 🍙🍙🍙{
+    func 🐶🐶(😇:Int, 😙: Int){
+        return 😙 + 😇
+    }
+}
+
+var 🍜 = 3
+var 🍝 = 🍜 + 5
+
+var 🍙 = 🍙🍙🍙()
+println(🍙.🐶🐶(🍜, 😙:🍝 ))
